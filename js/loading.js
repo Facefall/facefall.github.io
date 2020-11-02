0a220725811e726cc2bf5892c88cbd2646511425
CJy <3108999582@qq.com>
2020-11-02 20:00:53 +0800

    Site updated: 2020-11-02 20:00:53

--- js/loading.js
+++ js/loading.js
@@ -1,51 +1,97 @@
-document.addEventListener('DOMContentLoaded', function (){
-  'use strict';
-  console.log(123)
-  let load = document.createElement("DIV");
-  for(let i = 0; i < 3; i++){
-    load.appendChild(document.createElement("HR"))
+ 'use strict';
+let time1 = new Date();
+
+(function (){
+  switch (document.readyState){
+    case "loading":
+
+      let body = document.querySelector("body");
+      let head = document.querySelector("head");
+      let loadingBackground = document.createElement("DIV");
+      let load = document.createElement("DIV");
+      let style = document.createElement("STYLE");
+      for(let i = 0; i < 4; i++){
+        load.appendChild(document.createElement("HR"))
+      }
+
+      loadingBackground.style.cssText = `
+        position: absolute;
+        top:0;
+        z-index: 1;
+        width: 100vw;
+        height: 100vh;
+        background: white;
+        overflow:hidden;
+        transition:all .3s ease;
+      `;
+      loadingBackground.style.opacity = 1;
+      style.innerText = `
+        body{background:#ECF0F1}
+
+        .load{position:absolute;top:50%;left:50%;transform:translate(-50%, -50%);
+          /*change these sizes to fit into your project*/
+          width:100px;
+          height:100px;
+          opacity:1;
+          transition:opacity .3s ease;
+          z-index:10;
+        }
+        .load hr{border:0;margin:0;width:40%;height:40%;position:absolute;border-radius:50%;animation:spin 2s ease infinite}
+
+        .load :first-child{background:#19A68C;animation-delay:-1.5s}
+        .load :nth-child(2){background:#F63D3A;animation-delay:-1s}
+        .load :nth-child(3){background:#FDA543;animation-delay:-0.5s}
+        .load :last-child{background:#193B48}
+
+        @keyframes spin{
+          0%,100%{transform:translate(0)}
+          25%{transform:translate(160%)}
+          50%{transform:translate(160%, 160%)}
+          75%{transform:translate(0, 160%)}
+        }
+      `;
+      body.style.cssText = `height:100vh;background-color:white;overflow:hidden;`;
+      load.classList.add("load");
+      style.classList.add("loading-style");
+      loadingBackground.classList.add("loadingBackground")
+      body.appendChild(load);
+      body.appendChild(loadingBackground);
+      head.appendChild(style);
+      break;
   }
-  let style = document.createElement("STYLE");
+})();
 
-  load.classList.add("load");
-  style.classList.add("loading-style");
-  style.innerText = `
-  body{background:#ECF0F1}
+window.addEventListener("load", ()=> {
+  let time2 = new Date();
+  const endLoading = async (time = 0) => {
+    {
+      let loadingBackground = document.querySelector(".loadingBackground");
+      let load = document.querySelector(".load");
+      let body = document.querySelector("body");
+      let style = document.querySelector(".loading-style")
+      let head = document.querySelector("head");
+      console.log(3);
+      await new Promise(resolve => {
+        setTimeout(()=>{
+          load.style.opacity = 0;
+          loadingBackground.style.opacity = 0;
+        },time);
+      }).then((resolve)=>{
+        body.removeChild(load)
+        body.removeChild(loadingBackground)
+        head.removeChild(style)
+        console.log(2);
+      })
+    }
+  }
 
-.load{position:absolute;top:50%;left:50%;transform:translate(-50%, -50%);
-  /*change these sizes to fit into your project*/
-  width:100px;
-  height:100px;
-}
-.load hr{border:0;margin:0;width:40%;height:40%;position:absolute;border-radius:50%;animation:spin 2s ease infinite}
-
-.load :first-child{background:#19A68C;animation-delay:-1.5s}
-.load :nth-child(2){background:#F63D3A;animation-delay:-1s}
-.load :nth-child(3){background:#FDA543;animation-delay:-0.5s}
-.load :last-child{background:#193B48}
-
-@keyframes spin{
-  0%,100%{transform:translate(0)}
-  25%{transform:translate(160%)}
-  50%{transform:translate(160%, 160%)}
-  75%{transform:translate(0, 160%)}
-}
-  `;
-
-  let head = document.querySelector("head");
-  let body = document.querySelector("body");
-  head.appendChild(style)
-  body.appendChild(load)
+  if(time2 - time1 > 3000){
+    console.log(time2 - time1);
+    endLoading();
+  }
+  else{
+    endLoading(1000);
+  }
+  time1 = null;
 })
 
-document.addEventListener("load",function (){
-  'use strict';
-  setTimeout(()=>{
-    let load = document.querySelector("load");
-    let style = document.querySelector("loading-style");
-    let head = document.querySelector("head");
-    let body = document.querySelector("body");
-    head.removeChild(style);
-    body.removeChild(load);
-  },10000)
-})
